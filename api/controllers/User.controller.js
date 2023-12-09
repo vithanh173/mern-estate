0c1288602d779eff2b4d2e4fdc60f8d6fd5f1263
vithanh173 <jhjmhn@gmail.com>
2023-12-09 13:32:12 +0700

    create get user listing api route

--- api/controllers/User.controller.js
+++ api/controllers/User.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import User from "../models/User.model.js";
+import Listing from "../models/Listing.model.js";
 
 export const test = (req, res) => {
     res.json({ message: "Hello world!" });
@@ -48,5 +49,20 @@ export const signOut = async (req, res, next) => {
         res.status(200).json("User has been signed out");
     } catch (error) {
         next(error);
+    }
+};
+
+export const getUserListings = async (req, res, next) => {
+    console.log(req.params.id === req.user.id);
+    if (req.user.id === req.params.id) {
+        try {
+            const listings = await Listing.find({ userRef: req.params.id });
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    }
+    else {
+        return next(errorHandler(401, "You can only view your own listings!"));
     }
 }
--- api/routes/User.route.js
+++ api/routes/User.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, signOut, test, updateUser } from "../controllers/User.controller.js";
+import { deleteUser, getUserListings, signOut, test, updateUser } from "../controllers/User.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const userRouter = express.Router();
@@ -8,6 +8,7 @@ userRouter.get("/test", test);
 userRouter.post("/update/:id", verifyToken, updateUser);
 userRouter.delete("/delete/:id", verifyToken, deleteUser);
 userRouter.get("/signout", signOut);
+userRouter.get("/listings/:id", verifyToken, getUserListings);
 
 
 
